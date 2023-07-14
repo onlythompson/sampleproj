05ef7d73e5667a6506124b541afbf1d47f095f22
onlythompson <onlythompson@gmail.com>
2023-07-14 15:42:24 +0100

    Semantics Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 
+
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
 function App() {
   return (
-    <div className="App">
-     <p>Home Page</p>
-    </div>
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+const Footer = () => {
+  return (
+    <footer>
+        
+      <div>
+        <img src="" />
+      </div>
+
+      <div>
+        <h3>Doormat Navigation</h3>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Reservation</a>
+          </li>
+          <li>
+            <a href="#">Order Online</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3>Contact</h3>
+        <address>Address</address>
+        <p>Phone Number</p>
+        <p>Email</p>
+      </div>
+      <div>
+        <h3>Social Media Links</h3>
+        <address>Address</address>
+        <p>Phone Number</p>
+        <p>Email</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
